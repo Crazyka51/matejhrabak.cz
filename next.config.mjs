0a882313279910e26e3d2cb34d10171c56fa457f
Crazyka51 <145287796+Crazyka51@users.noreply.github.com>
2025-02-19 22:59:19 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,23 @@
 import mdx from '@next/mdx';
 import createNextIntlPlugin from 'next-intl/plugin';
 
+// Konfigurace MDX s rozšířením .mdx
 const withMDX = mdx({
     extension: /\.mdx?$/,
     options: { },
 });
 
-const withNextIntl = createNextIntlPlugin();
+// Konfigurace next-intl pro lokalizaci
+const withNextIntl = createNextIntlPlugin({
+    locales: ['en', 'cs'], // Přidejte všechny podporované jazyky
+    defaultLocale: 'en', // Výchozí jazyk
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // Rozšíření stránek, které Next.js bude zpracovávat
     pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
 };
 
+// Export kombinované konfigurace Next.js s MDX a lokalizací
 export default withNextIntl(withMDX(nextConfig));
