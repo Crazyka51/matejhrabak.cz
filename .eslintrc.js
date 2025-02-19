13d3a364069f1e274a1f4a08af487377b565292d
matej hrabakl <matejhrabak@gmail.com>
2025-02-19 22:28:24 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Přidání nových konfiguračních možností
+  overrideConfig: {
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+  },
+};
--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -1,76 +1,86 @@
-const baseURL = 'demo.magic-portfolio.com'
+const baseURL = "matejhrabak.cz";
 
 // Enable localization
-const i18n = false;
+const i18n = true;
 
 // Manage localized content in the messages folder
 const i18nOptions = {
-    locales: ['en'],            // A list of all locales that are supported, e.g. ['en','id']
-    defaultLocale: 'en'         // Locale used by default and as a fallback
-}
+  locales: ["cs"], // A list of all locales that are supported, e.g. ['en','id']
+  defaultLocale: "cs", // Locale used by default and as a fallback
+};
 
 const routes = {
-    '/':        true,
-    '/about':   true,
-    '/work':    true,
-    '/blog':    true,
-    '/gallery': true,
-}
+  "/": true,
+  "/about": true,
+  "/work": true,
+  "/blog": true,
+  "/gallery": true,
+};
 
 // Enable password protection on selected routes
 // Set password in pages/api/authenticate.ts
 const protectedRoutes = {
-    '/work/automate-design-handovers-with-a-figma-to-code-pipeline': true
-}
+  "/work/automate-design-handovers-with-a-figma-to-code-pipeline": true,
+};
 
 const effects = {
-    mask: 'cursor',             // none | cursor | topLeft | topRight | bottomLeft | bottomRight
-    gradient: {
-        display: true,
-        opacity: 0.4            // 0 - 1
-    },
-    dots: {
-        display: true,
-        opacity: 0.4,           // 0 - 1
-        size: '24'              // 2 | 4 | 8 | 12 | 16 | 20 | 24 | 32 | 40 | 48 | 56 | 634
-    },
-    lines: {
-        display: false,
-    },
-}
+  mask: "cursor", // none | cursor | topLeft | topRight | bottomLeft | bottomRight
+  gradient: {
+    display: true,
+    opacity: 0.8, // 0 - 1
+  },
+  dots: {
+    display: true,
+    opacity: 0.4, // 0 - 1
+    size: "24", // 2 | 4 | 8 | 12 | 16 | 20 | 24 | 32 | 40 | 48 | 56 | 634
+  },
+  lines: {
+    display: true,
+  },
+};
 
 const style = {
-    theme:       'dark',         // dark | light
-    neutral:     'gray',         // sand | gray | slate
-    brand:       'emerald',      // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-    accent:      'indigo',       // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-    solid:       'contrast',     // color | contrast
-    solidStyle:  'flat',         // flat | plastic
-    border:      'playful',      // rounded | playful | conservative
-    surface:     'translucent',  // filled | translucent
-    transition:  'all'           // all | micro | macro
-}
+  theme: "dark", // dark | light
+  neutral: "slate", // sand | gray | slate
+  brand: "red", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  accent: "cyan", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  solid: "color", // color | contrast
+  solidStyle: "flat", // flat | plastic
+  border: "playful", // rounded | playful | conservative
+  surface: "filled", // filled | translucent
+  transition: "all", // all | micro | macro
+};
 
 const display = {
-    location: true,
-    time:     true
-}
+  location: false,
+  time: true, // Zobrazení času
+};
 
 const mailchimp = {
-    action: 'https://url/subscribe/post?parameters',
-    effects: {
-        mask: 'topRight',           // none | cursor | topLeft | topRight | bottomLeft | bottomRight
-        gradient: {
-            display: true,
-            opacity: 0.6            // 0 - 1
-        },
-        dots: {
-            display: false,
-        },
-        lines: {
-            display: false,
-        },
-    }
-}
+  action: "https://url/subscribe/post?parameters",
+  effects: {
+    mask: "topRight", // none | cursor | topLeft | topRight | bottomLeft | bottomRight
+    gradient: {
+      display: true,
+      opacity: 0.6, // 0 - 1
+    },
+    dots: {
+      display: true,
+    },
+    lines: {
+      display: false,
+    },
+  },
+};
 
-export { routes, protectedRoutes, effects, style, display, mailchimp, baseURL, i18n, i18nOptions };
+export {
+  routes,
+  protectedRoutes,
+  effects,
+  style,
+  display,
+  mailchimp,
+  baseURL,
+  i18n,
+  i18nOptions,
+};
