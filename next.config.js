a64166c50d143c430a4fe936ba55e6658b150609
Crazyka51 <145287796+Crazyka51@users.noreply.github.com>
2025-02-19 23:31:25 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,15 @@
-import mdx from '@next/mdx';
-import createNextIntlPlugin from 'next-intl/plugin';
+const createNextIntlPlugin = require("next-intl/plugin")
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {},
+})
 
-// Konfigurace MDX s rozšířením .mdx
-const withMDX = mdx({
-    extension: /\.mdx?$/,
-    options: { },
-});
-
-// Konfigurace next-intl pro lokalizaci
-const withNextIntl = createNextIntlPlugin({
-    locales: ['en', 'cs'], // Přidejte všechny podporované jazyky
-    defaultLocale: 'cs', // Výchozí jazyk
-});
+const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Rozšíření stránek, které Next.js bude zpracovávat
-    pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
-};
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
+}
 
-// Export kombinované konfigurace Next.js s MDX a lokalizací
-export default withNextIntl(withMDX(nextConfig));
+module.exports = withNextIntl(withMDX(nextConfig))
+
