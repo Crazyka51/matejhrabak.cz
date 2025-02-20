4e355fd9e4baf1ad2342e494d46250c021b1fc8e
Crazyka51 <145287796+Crazyka51@users.noreply.github.com>
2025-02-20 17:17:26 +0000

    Opraven merge konflikt v next.config.js

--- next.config.js
+++ next.config.js
@@ -1,33 +1,16 @@
-import mdx from '@next/mdx';
-import createNextIntlPlugin from 'next-intl/plugin';
+import mdx from "@next/mdx";
+import createNextIntlPlugin from "next-intl/plugin";
 
 const withMDX = mdx({
-    extension: /\.mdx?$/,
-    options: { },
+  extension: /\.mdx?$/,
+  options: {},
 });
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
 };
 
 export default withNextIntl(withMDX(nextConfig));
-=======
-const createNextIntlPlugin = require("next-intl/plugin")
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {},
-})
-
-const withNextIntl = createNextIntlPlugin()
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ["ts", "tsx", "md", "mdx"],
-}
-
-module.exports = withNextIntl(withMDX(nextConfig))
-
->>>>>>> 0e42c8dfe75e176848d288e0a9ba00171ec69ae4
