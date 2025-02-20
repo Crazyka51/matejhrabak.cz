01a11e33363ed56ef6048a6df98424c9fa802c5c
Crazyka51 <145287796+Crazyka51@users.noreply.github.com>
2025-02-20 19:08:19 +0000

    Updated ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,8 @@
 module.exports = {
-  extends: [
-    "next/core-web-vitals",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  rules: {
-    "no-console": "warn",
-    "no-unused-vars": "warn",
+  // Přidání nových konfiguračních možností
+  overrideConfig: {
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
   },
 };
